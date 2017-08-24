95bcfe77044de9f6d00383cd39f853525900a427
e.guskov <e.guskov@gaijin.ru>
2017-08-24 13:48:51 +0300

    DX11: Disable D3D11_CREATE_DEVICE_DEBUG Scene: Fix paths according to my system

--- Atum/Services/Render/DX11/DeviceDX11.cpp
+++ Atum/Services/Render/DX11/DeviceDX11.cpp
@@ -91,7 +91,7 @@ bool DeviceDX11::Init(int width, int height, void* data)
 {
 	UINT createDeviceFlags = 0;
 #ifdef _DEBUG
-	createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+	// createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
 #endif
 
 	D3D_DRIVER_TYPE driverTypes[] =
